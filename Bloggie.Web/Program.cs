30dfe9f73a81de93764ad87dcb81fce1d59f4fe5
Dyy <mduy327@gmail.com>
2024-04-28 15:26:37 +0700

    Update like functionality

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -33,6 +33,8 @@ builder.Services.Configure<IdentityOptions>(options =>
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, CloudinaryImageRepository>();
+builder.Services.AddScoped<IBlogPostLikeRepository, BlogPostLikeRepository>();
+
 
 var app = builder.Build();
 
